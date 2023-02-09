d08e85d20bba5ff6cb73ee3127552a9a645ccdc2
arslonga83 <jlwood83@gmail.com>
2023-02-09 15:08:01 -0800

    refactor data structure into objects

--- script.js
+++ script.js
@@ -1,37 +1,157 @@
-const main = document.querySelector('#main')
+const textDisplay= document.querySelector('#textDisplay')
+const input = document.querySelector('#input')
 const speed = 50; 
+input.disabled = true
 let i = 0
+let textIndex = 0
+let currentId = 0
 
-const arrayOfText = [
-  'This is the first sentence.', 
-  'Here is something else.',
-  'This is part 3.'
+const adventureData = [
+  {
+    id: 0,
+    text: 'Welcome the the adventure! You wake up on a deserted beach. You can\'t remember how you got here, but there are bits of driftwood all around, seemingly from a shipwreck. In front of you looms a dense jungle and the shore stretches out as far as you can see in either direction. Do you go "left", "right", or "straight"?', 
+    options: [
+      {
+        choice: 'left',
+        nextId: 1
+      },
+      {
+        choice: 'right', 
+        nextId: 3
+      },
+      {
+        choice: 'straight', 
+        nextId: 2
+      }
+    ]
+  },
+  {
+    id: 1,
+    text: 'You proceed left and come to a rocky outcropping with a lighthouse. You don\'t see anyone around but the light is slowly turning. Do you "investigate" further or "continue" walking?', 
+    options: [
+      {
+        choice: 'left',
+        nextId: 23
+      },
+      {
+        choice: 'right', 
+        nextId: 11
+      },
+      {
+        choice: 'straight', 
+        nextId: 3
+      }
+    ]
+  },{
+    id: 2,
+    text: 'You make slow progress through the dense jungle foliage. The mosquitoes are ravenous and you are feeling worried about the plants you keep brushing against. Do you "keep going" or "retreat"?', 
+    options: [
+      {
+        choice: 'left',
+        nextId: 23
+      },
+      {
+        choice: 'right', 
+        nextId: 11
+      },
+      {
+        choice: 'straight', 
+        nextId: 3
+      }
+    ]
+  },{
+    id: 3,
+    text: 'The beach in this direction stretches on forever with no sign of any people or even animals. As the sun beats down you decide to stop and take a rest. Do you "sit on the beach" or "find a place to hide"?', 
+    options: [
+      {
+        choice: 'left',
+        nextId: 23
+      },
+      {
+        choice: 'right', 
+        nextId: 11
+      },
+      {
+        choice: 'straight', 
+        nextId: 3
+      }
+    ]
+  }
 ]
+
+
+const textToPrint = [
+  'Welcome the the adventure! You wake up on a deserted beach. You can\'t remember how you got here, but there are bits of driftwood all around, seemingly from a shipwreck. In front of you looms a dense jungle and the shore stretches out as far as you can see in either direction. Do you go "left", "right", or "straight"?',
+  'You proceed left and come to a rocky outcropping with a lighthouse. You don\'t see anyone around but the light is slowly turning. Do you "investigate" further or "continue" walking?',
+  'You make slow progress through the dense jungle foliage. The mosquitoes are ravenous and you are feeling worried about the plants you keep brushing against. Do you "keep going" or "retreat"?',
+  'The beach in this direction stretches on forever with no sign of any people or even animals. As the sun beats down you decide to stop and take a rest. Do you "sit on the beach" or "find a place to hide"?'
+]
+
+// function displayText() {
+//   textDisplay.innerHTML += `
+//     <br></br>
+//     `
+//     typeWriter(textToPrint[textIndex])
+// }
+
+function displayText() {
+  input.disabled = true
+  textDisplay.innerHTML += `
+    <br></br>
+    `
+    adventureData.map(data => {
+      if (data.id === currentId) {
+        typeWriter(data.text)
+      }
+    })
+}
 
 function typeWriter(text) {
   if (i < text.length) {
-    main.innerHTML += text.charAt(i);
+    textDisplay.innerHTML += text.charAt(i);
     i++;
     setTimeout(() => {
       typeWriter(text)
     }, speed);
   }
-  else (i = 0)
+  else {
+    i = 0
+    input.disabled = false
+    input.focus()
+  }
 }
 
+displayText()
 
-
-// document.addEventListener('keydown', (e) => {
-//   if (e.code === 'Space' && arrayOfText.length > 0) {
-//     main.innerHTML += `<br>`
-//     typeWriter(arrayOfText.shift())
-//   } else {
-//     main.innerHTML += `<br>`
-//     getInput()
+// input.addEventListener('keydown', (e) => {
+//   if (e.code === 'Enter') {
+//     let response = input.value
+//     input.value = ''
+//     if (response === 'left') {
+//       textIndex = 1
+//     } else if (response === 'straight') {
+//       textIndex = 2
+//     } else if (response === 'right') {
+//       textIndex = 3
+//     }
+//     displayText()
 //   }
 // })
 
-// function getInput() {
-//   main.innerHTML += `
-//   <input type="text">`
-// }
+input.addEventListener('keydown', (e) => {
+  console.log('click')
+  if (e.code === 'Enter') {
+    adventureData.map(data => {
+      if (data.id === currentId) {
+        data.options.map(option => {
+          if (option.choice === input.value) {
+            currentId = option.nextId
+            input.value = ''
+            displayText()
+          }
+        })
+      }
+    })
+  }
+})
+
